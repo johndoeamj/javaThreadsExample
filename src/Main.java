394208f8f1a3f3200f72b919d945c63abdc5fdaa
johndoeamj <johndoeamj@gmail.com>
2024-08-02 14:03:06 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Thread " + Thread.currentThread().getId() + " is running");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Both threads have finished.");
+    }
+}
